c224b30af03c79fd2b5c219e0d16bcea829a52bc
Oisín <oinolan@tcd.ie>
2019-03-07 10:24:24 +0000

    Accents working
    API can now parse urls with accented characters in them.
    
    Created helper function which returns a dictionary containing word and list of grammatical attributes.

--- main.py
+++ main.py
@@ -1,58 +1,85 @@
+# Imports
+
 import urllib.request
+import urllib.parse
 import re
 from bs4 import BeautifulSoup
 
-print("Enter a French word:")
-searchWord = input().lower()
-searchWordForURL = searchWord.replace(" ", "%20")
+# Helper functions
 
-url = 'http://www.wordreference.com/fren/' + searchWordForURL
+def getWordData(word):
+    searchWord = word.lower()
+    searchWordForURL = searchWord.replace(" ", "%20")
 
-req = urllib.request.Request(
-    url, 
-    data=None, 
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    }
-)
+    url = 'http://www.wordreference.com/fren/' + urllib.parse.quote(searchWordForURL)
 
-byteData = urllib.request.urlopen(req)
-stringData = byteData.read().decode('utf8')
+    req = urllib.request.Request(
+        url, 
+        data=None, 
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }
+    )
 
-soup = BeautifulSoup(stringData, features="lxml")
-data = []
-noEntryFound = soup.find('p', attrs={'id':'noEntryFound'})
+    byteData = urllib.request.urlopen(req)
+    stringData = byteData.read().decode('utf8')
 
-if not noEntryFound:
-    table = soup.find('table', attrs={'class':'WRD'})
+    soup = BeautifulSoup(stringData, features="lxml")
+    data = []
+    noEntryFound = soup.find('p', attrs={'id':'noEntryFound'})
 
-    rows = table.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])
+    if not noEntryFound:
+        table = soup.find('table', attrs={'class':'WRD'})
 
-    searchWordText = ""
-    wordDict = {}
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            data.append([ele for ele in cols if ele])
 
-    wordMatch = False
+        searchWordText = ""
+        wordDict = {}
+        wordDict.setdefault(searchWord, [])
 
-    for list in data:
-        for text in list:
-            if text[:len(searchWord)] == searchWord and not wordMatch:
-                wordDict[searchWord] = text.split()[1][:-3]
-                wordMatch = True
-                // only match word if it actually is a nom!
+        wordMatch = False
 
-    genderCodes = {
-        'nm' : 'masculine',
-        'nf' : 'feminine',
-        'nmpl' : 'masculine plural',
-        'nfpl' : 'feminine plural',
-    }
+        # need to add more codes here, ie pron, nmf
+        grammarCodes = {
+            'nmnom' : 'masculine noun',
+            'nfnom' : 'feminine noun',
+            'mfnom' : 'masculine/feminine noun',
+            'mplnom' : 'masculine plural noun',
+            'fplnom' : 'feminine plural noun',
+            'adj' : 'adjective',
+            'vtrverbe' : 'transitive verb',
+            'viverbe' : 'intransitive verb',
+            'interjinterjection' : 'interjection',
+            'pron' : 'pronoun',
+        }
 
-    output = searchWord + " is " + genderCodes[wordDict[searchWord]]
-    print(output)
+        for list in data:
+            for text in list:
+                if text.split()[0] == searchWord:
+                    textGrammarCode = text.split()[1]
+                    print(textGrammarCode)
+                    for code in grammarCodes:
+                        if code in textGrammarCode and grammarCodes[code] not in wordDict[searchWord]:
+                            wordDict.setdefault(searchWord, []).append(grammarCodes[code])
 
-else:
-    print("Sorry, word not found!")
+        return wordDict
+
+    else:
+        return None
+
+# API functions
+
+def getGender(word):
+    wordDict = getWordData(word)
+    genderCodes = ['masculine noun', 'feminine noun', 'masculine plural noun', 'feminine plural noun', 'masculine/feminine noun']
+    wordGenders = []
+
+    for code in wordDict[word]:
+        if code in genderCodes:
+            wordGenders.append(code)
+
+    return wordGenders
